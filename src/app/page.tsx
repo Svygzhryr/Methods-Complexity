b29d469ab215ebb410721d335d9b22ba367b349f
Pavel Mihailov <pamixy@gmail.com>
2025-02-12 14:10:46 +0300

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import styles from "./page.module.css";
+
+export default function Page() {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1 className={styles.title}>JS methods complexity</h1>
+      </header>
+      <main>
+        <h2></h2>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
